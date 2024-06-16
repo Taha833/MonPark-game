a9e6309108c88aa604cd6e7bc0b96442760f2cb5
Nimish22211 <shailbandha@gmail.com>
2024-06-16 21:22:36 +0530

    redo changes for production

--- game/src/index.js
+++ game/src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-if ('serviceWorker' in navigator) {
-  alert('service worker reg')
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-} else {
-  alert('service worker not reg')
-  console.log('no service worker')
-}
+// if ('serviceWorker' in navigator) {
+//   alert('service worker reg')
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('/service-worker.js')
+//       .then(registration => {
+//         console.log('Service Worker registered:', registration);
+//       })
+//       .catch(error => {
+//         console.error('Service Worker registration failed:', error);
+//       });
+//   });
+// } else {
+//   alert('service worker not reg')
+//   console.log('no service worker')
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
