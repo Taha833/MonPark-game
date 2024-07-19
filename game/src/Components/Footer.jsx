d77a695e90316654ed0c4633fe315941e0ed1a70
Nimish22211 <shailbandha@gmail.com>
2024-07-19 23:57:04 +0530

    footer & header updated

--- game/src/Components/Footer.jsx
+++ game/src/Components/Footer.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
-        <nav className='flex justify-between max-w-[325px] mx-auto items-center w-full p-2'>
-            <div className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="assets/game/house.svg" alt="house" />
+        <nav className='flex justify-between max-w-[325px] mx-auto items-center w-full p-2 sticky bottom-0 z-10 bg-ft-gradient'>
+            <Link to="/house" className='text-white flex flex-col items-center cursor-pointer'>
+                <img src="/assets/game/house.svg" alt="house" />
                 <span>House</span>
-            </div>
-            <div className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="assets/game/shop.svg" alt="shop" />
+            </Link>
+            <Link to="/shop/product" className='text-white flex flex-col items-center cursor-pointer'>
+                <img src="/assets/game/shop.svg" alt="shop" />
                 <span>Shop</span>
-            </div>
-            <div className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="assets/game/earn-coin.svg" alt="earn-coin" />
+            </Link>
+            <Link to="/earn" className='text-white flex flex-col items-center cursor-pointer'>
+                <img src="/assets/game/earn-coin.svg" alt="earn-coin" />
                 <span>Earn</span>
-            </div>
-            <div className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="assets/game/friends.svg" alt="friends" />
+            </Link>
+            <Link to="/friends" className='text-white flex flex-col items-center cursor-pointer'>
+                <img src="/assets/game/friends.svg" alt="friends" />
                 <span>Friends</span>
-            </div>
+            </Link>
         </nav>
     )
 }
--- game/src/Components/Header.jsx
+++ game/src/Components/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import useUserData from '../Hooks/useUserData'
 
 function Header() {
+    const { userData } = useUserData()
+
     return (
         <div>
             <div className='text-white flex justify-center gap-3 pt-16 w-full max-w-[325px] mx-auto text-[12px] text-center'>
@@ -8,28 +11,28 @@ function Header() {
                     <span>
                         Feed per tap
                     </span>
-                    <div className='flex gap-2 justify-center'><img src="assets/game/pajamas_food.svg" alt="feed" />-3</div>
+                    <div className='flex gap-2 justify-center'><img src="/assets/game/pajamas_food.svg" alt="feed" />+{userData.foodPerTap}</div>
                 </div>
                 <div className='bg-[#421F78] rounded-2xl p-3'>
                     <span>
-                        Feed to hatch
+                        Feed to {userData.level === 0 ? 'hatch' : 'grow'}
                     </span>
-                    <div className='flex gap-2 justify-center'><img src="assets/game/pajamas_food.svg" alt="feed" />10k</div>
+                    <div className='flex gap-2 justify-center'><img src="/assets/game/pajamas_food.svg" alt="feed" />{userData.feedToNextLevel}</div>
                 </div>
                 <div className='bg-[#421F78] rounded-2xl p-3'>
                     <span>
                         Income per hour
                     </span>
-                    <div className='flex gap-2 justify-center'><img src="assets/game/tabler_coin-filled.svg" alt="feed" />+2.1k</div>
+                    <div className='flex gap-2 justify-center'><img src="/assets/game/tabler_coin-filled.svg" alt="feed" />+{userData.incomePerHour}</div>
                 </div>
             </div>
 
             <div className='text-3xl text-white flex mt-6 justify-between max-w-[325px] mx-auto w-full'>
                 <div>
-                    <div className='flex items-center gap-4'><img src="assets/game/pajamas_food.svg" alt="feed" width='50' />7024</div>
+                    <div className='flex items-center gap-4'><img src="assets/game/pajamas_food.svg" alt="feed" width='50' />{userData.foodLeft}</div>
                 </div>
                 <div>
-                    <div className='flex items-center gap-4'><img src="assets/game/tabler_coin-filled.svg" alt="feed" width='50' />7024</div>
+                    <div className='flex items-center gap-4'><img src="/assets/game/tabler_coin-filled.svg" alt="feed" width='50' />{userData.totalIncome}</div>
                 </div>
             </div>
         </div>
