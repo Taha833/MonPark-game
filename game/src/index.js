4813d914fef09fdd88e7b8aa1cf66068c3598c0e
Nimish22211 <shailbandha@gmail.com>
2024-07-19 17:47:17 +0530

    service worker + react context wrapper

--- game/src/index.js
+++ game/src/index.js
@@ -2,28 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import UserProviderWrapper from './UserProviderWrapper';
 
-// if ('serviceWorker' in navigator) {
-//   alert('service worker reg')
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker
-//       .register('/service-worker.js')
-//       .then(registration => {
-//         console.log('Service Worker registered:', registration);
-//       })
-//       .catch(error => {
-//         console.error('Service Worker registration failed:', error);
-//       });
-//   });
-// } else {
-//   alert('service worker not reg')
-//   console.log('no service worker')
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+} else {
+  alert('service worker not reg')
+  console.log('no service worker')
+}
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <App />
+  <Router>
+    <UserProviderWrapper>
+      <App />
+    </UserProviderWrapper>
+  </Router>
   // </React.StrictMode>
 );
 
