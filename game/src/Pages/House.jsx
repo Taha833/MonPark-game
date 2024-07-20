ef93faa51bbcb76ff051ea70b38b9c0a054f3db8
Nimish22211 <shailbandha@gmail.com>
2024-07-20 16:42:10 +0530

    level upgrade, income per hour and client data update

--- game/src/Pages/House.jsx
+++ game/src/Pages/House.jsx
@@ -1,142 +1,186 @@
 import React, { useEffect, useRef, useState } from 'react'
+import useUserData from '../Hooks/useUserData';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { Link } from 'react-router-dom';
+function House({ server }) {
 
-function House({ progress }) {
-
-    progress = 20
-    const progressValue = Math.min(Math.max(progress, 0), 100);
-
-    const [data, setData] = useState(0);
+    const { userData, setUserData, initLoad, setInitLoad } = useUserData()
     const dataRef = useRef(0)
+    const [incomeModal, setIncomeModal] = useState(false)
+    const [income, setIncome] = useState(0)
+
     // Update local data
     const handleDataChange = () => {
-        dataRef.current = dataRef.current + 1
-
+        setUserData(prev => ({
+            ...prev, foodEaten: prev.foodEaten + prev.foodPerTap, foodLeft: prev.foodLeft - prev.foodPerTap
+        }))
     };
 
-    useEffect(() => {
-        const handleBeforeUnload = (event, type) => {
-            event.preventDefault();
-            event.returnValue = ''; // Required for Chrome
-            const txt = document.createElement('div')
-            txt.innerText = 'Service Worker'
-            console.log('reload')
-            // Send data to the service worker
-            if (navigator.serviceWorker) {
-                navigator.serviceWorker.controller.postMessage({ type, data: dataRef.current });
-                console.log('nav')
-            }
-        };
-
-        fetch('https://monpark-game-production.up.railway.app/api', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                data
-            }),
+    const notify = (content) => {
+        toast.success('Your pet is now! ' + content + ' old', {
+            position: "top-right",
+            autoClose: 5000,
+            closeOnClick: true,
+            theme: "dark",
         });
 
-
-        document.addEventListener('visibilitychange', e => {
-            if (document.visibilityState === "hidden") {
-                handleBeforeUnload(e, 'visibility')
-            } else {
-                handleBeforeUnload(e, 'still active')
-
-            }
+    }
+    const handleIncome = async () => {
+        console.log('func')
+        await fetch(server + '/income', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userData })
+        }).then(res => res.json()).then(data => {
+            setIncomeModal(true)
+            setIncome(data.incomeGenerated)
         })
-
-
-        window.addEventListener('beforeunload', e => handleBeforeUnload(e, 'beforeunload'));
-
-        window.addEventListener('pagehide', e => handleBeforeUnload(e, 'pagehide'))
-        window.addEventListener('unload', e => handleBeforeUnload(e, 'unload'))
-
-        return () => {
-            window.removeEventListener('beforeunload', e => handleBeforeUnload(e, 'visibility'));
-            document.removeEventListener('visibilitychange', e => handleBeforeUnload(e, 'beforeunload'))
-            window.removeEventListener('pagehide', e => handleBeforeUnload(e, 'pagehide'))
-            window.removeEventListener('unload', e => handleBeforeUnload(e, 'unload'))
-        };
-    }, [data]);
-
+    }
 
     useEffect(() => {
-        console.log(dataRef.current)
-    }, [])
+        console.log('loaddata ', initLoad)
+        if (initLoad) {
+            handleIncome()
+            setInitLoad(false)
+        }
+        //eslint-disable-next-line
+    }, [initLoad])
+
+    useEffect(() => {
+        if (userData.foodEaten !== undefined && userData.feedToNextLevel !== undefined && userData.foodEaten >= userData.feedToNextLevel) {
+            fetch(server + '/upgradeLevel', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(userData)
+            }).then(res => res.json()).then((data) => {
+                console.log(data)
+                setUserData(data.mergedData)
+                notify(data.mergedData.level)
+            })
+        }
+
+        //eslint-disable-next-line
+    }, [userData])
+
 
     useEffect(() => {
         const egg = document.getElementById('egg')
+        const creature = document.getElementById('creature')
+        if ((!incomeModal && egg) || (!incomeModal && creature)) {
+            console.log('event init')
+            egg.addEventListener('touchstart', e => {
+                egg.classList.add('scale-95')
+                console.log("touches", e.touches.length)
+                console.log("targets", e.targetTouches.length)
+                console.log("changed", e.changedTouches.length)
+                handleDataChange()
+                console.log('this is data', userData)
 
-        egg.addEventListener('touchstart', e => {
-            console.log("touches", e.touches.length)
-            console.log("targets", e.targetTouches.length)
-            console.log("changed", e.changedTouches.length)
-            handleDataChange()
-            setData(prev => prev + 1)
-        })
-
-        document.addEventListener('touchstart', e => {
-            ;[...e.changedTouches].forEach(touch => {
-                const dot = document.createElement('div')
-                dot.classList.add('bg-red-500')
-                dot.classList.add('w-3')
-                dot.classList.add('h-3')
-                dot.classList.add('absolute')
-                dot.style.top = `${touch.pageY}px`
-                dot.style.left = `${touch.pageX}px`
-                dot.id = touch.identifier
-
-                document.body.append(dot)
             })
-            // console.log(e)
-        })
 
-        document.addEventListener('touchend', e => {
-            ;[...e.changedTouches].forEach(touch => {
-                const dot = document.getElementById(touch.identifier)
-                dot.remove()
+            egg.addEventListener('touchend', e => {
+                egg.classList.remove('scale-95')
+
             })
-        })
 
-        document.addEventListener('touchcancel', e => {
-            ;[...e.changedTouches].forEach(touch => {
-                const dot = document.getElementById(touch.identifier)
-                dot && dot.remove()
+            document.addEventListener('touchstart', e => {
+                ;[...e.changedTouches].forEach(touch => {
+                    const dot = document.createElement('div')
+                    dot.classList.add('bg-red-500')
+                    dot.classList.add('w-3')
+                    dot.classList.add('h-3')
+                    dot.classList.add('absolute')
+                    dot.style.top = `${touch.pageY}px`
+                    dot.style.left = `${touch.pageX}px`
+                    dot.id = touch.identifier
+
+                    document.body.append(dot)
+                })
             })
-        })
 
-    }, [])
+            document.addEventListener('touchend', e => {
+                ;[...e.changedTouches].forEach(touch => {
+                    const dot = document.getElementById(touch.identifier)
+                    dot.remove()
+                })
+            })
+
+            document.addEventListener('touchcancel', e => {
+                ;[...e.changedTouches].forEach(touch => {
+                    const dot = document.getElementById(touch.identifier)
+                    dot && dot.remove()
+                })
+            })
+
+        }
+
+        return () => {
+
+        }
+        //eslint-disable-next-line
+    }, [incomeModal])
+
+    const handleIncomeUpdate = () => {
+        setUserData({ ...userData, totalIncome: userData.totalIncome + income })
+        setIncomeModal(false)
+    }
+
     return (
-        <div className='h-full justify-between flex flex-col items-start'>
-            <div className='max-w-[325px] mx-auto mt-8 w-full'>
-                <div className='text-white flex justify-between'>
-                    <span>Egg</span>
-                    <span>Hatch</span>
-                </div>
-                <progress
-                    className=" w-full h-2"
-                    value={progressValue}
-                    max="100"
+        <div className={`h-full justify-between flex flex-col ${incomeModal ? 'items-center' : 'items-start'}`}>
+            <ToastContainer />
+            {!incomeModal &&
+                <>
+                    <div className='max-w-[325px] mx-auto mt-8 w-full relative'>
+                        <div className='text-white flex justify-between mb-1'>
+                            <span>{userData.level === 0 ? 'Egg' : userData.level + ' years old'}</span>
+                            <span>{userData.level === 0 ? 'Hatch' : '+1'}</span>
+                        </div>
+                        <div className="relative w-full h-3">
+                            <progress
+                                className="absolute top-0 left-0 w-full h-full"
+                                value={userData.foodEaten}
+                                max={userData.feedToNextLevel}
+                            />
+                            <span className="absolute left-1/2 text-white z-50 text-[12px]" style={{ top: '50%', transform: 'translate(-50%, -50%)' }}>
+                                {userData.foodEaten}/{userData.feedToNextLevel}
+                            </span>
+                        </div>
+                    </div>
+                    <div className='mx-auto flex justify-center transition-transform duration-200' id="egg">
+                        {userData.level === 0 ? <img src="/assets/game/egg.svg" alt="egg" /> :
+                            <img src={`assets/game/pets/${userData.petAssigned}.svg`} alt="creature" id="creature" />
+                        }
+                    </div>
 
-                />
-            </div>
-
-            <div className='mx-auto flex justify-center' id="egg">
-                <img src="assets/game/egg.svg" alt="egg" />
-            </div>
-
-            <div className='flex max-w-[325px] mx-auto justify-between w-full'>
-                <div className='text-white flex gap-3'>
-                    <img src="assets/game/tabler_bowl-spoon.svg" alt="bowl" />
-                    <span>{dataRef.current} / 500</span>
+                    <div className='flex max-w-[325px] mx-auto justify-between w-full'>
+                        <div className='text-white flex gap-3'>
+                            <img src="assets/game/tabler_bowl-spoon.svg" alt="bowl" />
+                            <span>{dataRef.current} / 500</span>
+                        </div>
+                        <div className='text-white flex gap-3'>
+                            <img src="assets/game/kitchen.svg" alt="bowl" />
+                            <Link to="/kitchen">Kitchen</Link>
+                        </div>
+                    </div>
+                </>
+            }
+            {incomeModal && <div className='fixed max-w-[325px] text-white text-center border-[#492188] border-2 flex flex-col gap-3 items-center py-4 w-full mt-2' onClick={handleIncomeUpdate}>
+                <img src='/assets/cross.png' alt="cross" width="25px" className='absolute right-0 cursor-pointer top-0 m-2' />
+                <img src="/assets/monpark logo.svg" alt="monpark" width="80px" />
+                <div className='flex'>
+                    <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="40px" />
+                    <h1 className='text-4xl'>{income}</h1>
                 </div>
-                <div className='text-white flex gap-3'>
-                    <img src="assets/game/kitchen.svg" alt="kitchen" />
-                    <span>Kitchen</span>
+                <div>
+                    You are earning money!
                 </div>
-            </div>
+                <button className='bg-[#492188] py-2 px-4 text-xl cursor- rounded-md w-max hover:bg-[#8d5ae0]' onClick={handleIncomeUpdate}>
+                    Thank you, MonPark ♥
+                </button>
+            </div>}
         </div>
     )
 }
