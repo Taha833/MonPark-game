c45fb7328608b2dcd51da7783fd31556059e0959
Nimish22211 <shailbandha@gmail.com>
2024-08-11 00:10:05 +0530

    added links to earn section

--- game/src/Pages/Earn.jsx
+++ game/src/Pages/Earn.jsx
@@ -22,24 +22,13 @@ function Earn() {
                     <div className='flex flex-col gap-3'>
                         <span>Social Tasks</span>
                         <div className='flex flex-col gap-2'>
-                            <div className='flex bg-[#321B55] rounded-lg px-2 py-2 gap-2 items-center'>
-                                <img src="/assets/twitter logo.svg" alt="twitter" width='30px' />
-                                <div className='flex-col'>
-                                    <span>Follow our X</span>
-                                    <div className='flex gap-1 items-center'>
-                                        <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="20px" />
-
-                                        <span>+15</span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-col gap-2">
-                                <div className='bg-[#321B55] rounded-lg px-2 py-2 flex gap-2'>
-                                    <img src="/assets/telegram logo.svg" alt="telegram" width='30px' />
+                            <div className='flex bg-[#321B55] rounded-lg px-2 py-2 gap-2 items-center justify-between' >
+                                <div className='flex gap-2 w-full' onClick={() => {
+                                    window.Telegram.WebApp.openLink('https://twitter.com/intent/follow?screen_name=MonPark_bot')
+                                }}>
+                                    <img src="/assets/twitter logo.svg" alt="twitter" width='30px' />
                                     <div className='flex-col'>
-
-                                        <span>Join Telegram channel</span>
+                                        <span>Follow our X</span>
                                         <div className='flex gap-1 items-center'>
                                             <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="20px" />
 
@@ -47,33 +36,67 @@ function Earn() {
                                         </div>
                                     </div>
                                 </div>
+                                <div>Check</div>
                             </div>
+
                             <div className="flex flex-col gap-2">
-                                <div className='bg-[#321B55] rounded-lg px-2 py-2 flex gap-2'>
-                                    <img src="#" alt="" />
-                                    <div className='flex-col'>
+                                <div className='bg-[#321B55] rounded-lg px-2 py-2 flex gap-2 items-center justify-between'>
+                                    <div className='flex gap-2 w-full' onClick={() => {
+                                        window.Telegram.WebApp.openLink('https://t.me/monpark')
+                                    }}>
+                                        <img src="/assets/telegram logo.svg" alt="telegram" width='30px' />
+                                        <div className='flex-col'>
 
-                                        <span>Tweet about us</span>
-                                        <div className='flex gap-1 items-center'>
-                                            <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="20px" />
+                                            <span >Join Telegram channel</span>
+                                            <div className='flex gap-1 items-center'>
+                                                <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="20px" />
 
-                                            <span>+15</span>
+                                                <span>+15</span>
+                                            </div>
                                         </div>
                                     </div>
+                                    <div>Check</div>
+
                                 </div>
                             </div>
                             <div className="flex flex-col gap-2">
-                                <div className='bg-[#321B55] rounded-lg px-2 py-2 flex gap-2'>
-                                    <img src="#" alt="" />
-                                    <div className='flex-col'>
+                                <div className='bg-[#321B55] rounded-lg px-2 py-2 flex gap-2 items-center justify-between'>
+                                    <div className='flex gap-2 w-full' onClick={() => {
 
-                                        <span>Invite 2 friends</span>
-                                        <div className='flex gap-1 items-center'>
-                                            <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="20px" />
+                                        window.Telegram.WebApp.openLink('https://twitter.com/intent/tweet?text=Your%20message%20here')
+                                    }}>
+                                        <img src="#" alt="" />
+                                        <div className='flex-col'>
 
-                                            <span>+15</span>
+                                            <span>Tweet about us</span>
+                                            <div className='flex gap-1 items-center'>
+                                                <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="20px" />
+
+                                                <span>+15</span>
+                                            </div>
                                         </div>
                                     </div>
+                                    <div>Check</div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <div className='bg-[#321B55] rounded-lg px-2 py-2 flex gap-2 items-center justify-between'>
+                                    <div className='flex gap-2 w-full' onClick={() => {
+
+                                        window.Telegram.WebApp.openLink('https://twitter.com/intent/tweet?text=Your%20message%20here')
+                                    }}>
+                                        <img src="#" alt="" />
+                                        <div className='flex-col'>
+
+                                            <span>Invite 2 friends</span>
+                                            <div className='flex gap-1 items-center'>
+                                                <img src="/assets/game/tabler_coin-filled.svg" alt="coin" width="20px" />
+
+                                                <span>+15</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>Check</div>
                                 </div>
                             </div>
 
