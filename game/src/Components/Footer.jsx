31ff09938db8cac4fc5d85e82cf15ed0a27e4373
Nimish22211 <shailbandha@gmail.com>
2024-08-15 21:05:21 +0530

    footer logo color change based on location

--- game/src/Components/Footer.jsx
+++ game/src/Components/Footer.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { HouseSvg } from '../SVG/HouseSvg';
+import { ShopSvg } from '../SVG/ShopSvg';
+import { EarnSvg } from '../SVG/EarnSvg';
+import { FriendsSvg } from '../SVG/FriendsSvg';
 
 function Footer() {
+    const location = useLocation()
+    const getColor = (path) => location.pathname.includes(path) ? '#9654FF' : '#888888';
+
     return (
         <nav className='flex justify-between max-w-[325px] mx-auto items-center w-full p-2 sticky bottom-0 z-10 bg-ft-gradient'>
             <Link to="/house" className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="/assets/game/house.svg" alt="house" />
+                <HouseSvg color={getColor('/house')} />
                 <span>House</span>
             </Link>
             <Link to="/shop/product" className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="/assets/game/shop.svg" alt="shop" />
+                <ShopSvg color={getColor('/shop')} />
                 <span>Shop</span>
             </Link>
             <Link to="/earn" className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="/assets/game/earn-coin.svg" alt="earn-coin" />
+                <EarnSvg color={getColor('/earn')} />
                 <span>Earn</span>
             </Link>
             <Link to="/friends" className='text-white flex flex-col items-center cursor-pointer'>
-                <img src="/assets/game/friends.svg" alt="friends" />
+                <FriendsSvg color={getColor('/friends')} />
                 <span>Friends</span>
             </Link>
         </nav>
